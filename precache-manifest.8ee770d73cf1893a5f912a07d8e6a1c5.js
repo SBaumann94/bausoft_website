b83ee6b07dcb2fd87325653998cc781425b8e703
BotMan <baumann.istvan94@gmail.com>
2020-09-07 10:08:53 +0200

    Updates

---
+++ precache-manifest.8ee770d73cf1893a5f912a07d8e6a1c5.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dac4b209627d32f6fefe93663077333",
+    "url": "/bausoft_website/index.html"
+  },
+  {
+    "revision": "ce48f34ccb88ce217674",
+    "url": "/bausoft_website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c44146782219c7ed0729",
+    "url": "/bausoft_website/static/css/main.e032d619.chunk.css"
+  },
+  {
+    "revision": "ce48f34ccb88ce217674",
+    "url": "/bausoft_website/static/js/2.70009289.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bausoft_website/static/js/2.70009289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44146782219c7ed0729",
+    "url": "/bausoft_website/static/js/main.146f4e38.chunk.js"
+  },
+  {
+    "revision": "7b471122d30d46a64da5",
+    "url": "/bausoft_website/static/js/runtime-main.dee9e1c3.js"
+  },
+  {
+    "revision": "071a5e28bc73ea89d47512b7e9476fcb",
+    "url": "/bausoft_website/static/media/chm-bau2.071a5e28.gif"
+  },
+  {
+    "revision": "a3d7b5046cb689a0d90c830a7733bb8c",
+    "url": "/bausoft_website/static/media/chm-bau3.a3d7b504.gif"
+  },
+  {
+    "revision": "d3aa86148d7d43740289b8b38e9fb040",
+    "url": "/bausoft_website/static/media/chm-bau32_1.d3aa8614.gif"
+  },
+  {
+    "revision": "2e38c0aa0642fd1f5df624c0d40de787",
+    "url": "/bausoft_website/static/media/chm-bau32_2.2e38c0aa.gif"
+  },
+  {
+    "revision": "28b42a918595461b61cbc0645639be00",
+    "url": "/bausoft_website/static/media/chm-bau32_3.28b42a91.gif"
+  },
+  {
+    "revision": "9766948536973b347f4c74730fec5e05",
+    "url": "/bausoft_website/static/media/chm-bau32_4.97669485.gif"
+  },
+  {
+    "revision": "779e0ef4de4e610e897feabfff746da4",
+    "url": "/bausoft_website/static/media/chm-bau32_5.779e0ef4.gif"
+  },
+  {
+    "revision": "4880c409809f7551f1a90b9fd9b12827",
+    "url": "/bausoft_website/static/media/chm-bau32_6.4880c409.gif"
+  },
+  {
+    "revision": "0788d125c2dbded5aa4a89f8aa24581f",
+    "url": "/bausoft_website/static/media/cinege.0788d125.gif"
+  },
+  {
+    "revision": "4f895816cf11ea391467d6c37e499b8a",
+    "url": "/bausoft_website/static/media/englishegg.4f895816.png"
+  },
+  {
+    "revision": "5d6b09d029e51f79017ce8076fd87a82",
+    "url": "/bausoft_website/static/media/epbdegg.5d6b09d0.png"
+  },
+  {
+    "revision": "d44f9ef4038414eab4306974167c223a",
+    "url": "/bausoft_website/static/media/fecske.d44f9ef4.gif"
+  },
+  {
+    "revision": "b4b87745887b38b04fa95d0cce499baa",
+    "url": "/bausoft_website/static/media/fluctuvent.b4b87745.png"
+  },
+  {
+    "revision": "52f4313af172212c3047ff9831f59dab",
+    "url": "/bausoft_website/static/media/gasnet2.52f4313a.gif"
+  },
+  {
+    "revision": "17dff8457704c0578b7f60a2c627cf55",
+    "url": "/bausoft_website/static/media/gasnet3.17dff845.gif"
+  },
+  {
+    "revision": "4780aafe6da654a8d099677f8ca7d9a5",
+    "url": "/bausoft_website/static/media/gasnet32_1.4780aafe.gif"
+  },
+  {
+    "revision": "4619cfc7301a309d7de7339f43fb6478",
+    "url": "/bausoft_website/static/media/gasnet32_2.4619cfc7.gif"
+  },
+  {
+    "revision": "081cc209dcf06f62b91f9c2f56845db7",
+    "url": "/bausoft_website/static/media/gasnet32_3.081cc209.gif"
+  },
+  {
+    "revision": "b3ff306d686512b7e3e8d7741294f5dc",
+    "url": "/bausoft_website/static/media/gbxmlegg.b3ff306d.png"
+  },
+  {
+    "revision": "75cd0ca53b98ee7ca39bc07af2ee575e",
+    "url": "/bausoft_website/static/media/golya.75cd0ca5.gif"
+  },
+  {
+    "revision": "d2fb56d086a78f3c38a28323b06b0665",
+    "url": "/bausoft_website/static/media/hocsere_1.d2fb56d0.gif"
+  },
+  {
+    "revision": "5d08c5f09b842e2db29b7580cf98a98c",
+    "url": "/bausoft_website/static/media/hocsere_3.5d08c5f0.gif"
+  },
+  {
+    "revision": "057f2ddbf8ff887eabf34ad6d2de1147",
+    "url": "/bausoft_website/static/media/hocsere_4.057f2ddb.gif"
+  },
+  {
+    "revision": "e176ea85171d65ce950d66e0bad5c2a3",
+    "url": "/bausoft_website/static/media/hollo.e176ea85.gif"
+  },
+  {
+    "revision": "6f3f929a2b8f6d57ae373857f17a11f5",
+    "url": "/bausoft_website/static/media/hurkolt_2.6f3f929a.gif"
+  },
+  {
+    "revision": "173c9807ba4e538d53b50e9e95d297e3",
+    "url": "/bausoft_website/static/media/isobau_2.173c9807.gif"
+  },
+  {
+    "revision": "6e4c8ceb23aa8e152fd5c26b727ca5cf",
+    "url": "/bausoft_website/static/media/isobau_3.6e4c8ceb.gif"
+  },
+  {
+    "revision": "aa4ee03ce154caf16a4338de3da73d8e",
+    "url": "/bausoft_website/static/media/isobau_4.aa4ee03c.gif"
+  },
+  {
+    "revision": "2f4d447ccd30b5fb70f45b55c47173ed",
+    "url": "/bausoft_website/static/media/kocsag.2f4d447c.gif"
+  },
+  {
+    "revision": "403b6680b64ea4afa22e42fa7667a589",
+    "url": "/bausoft_website/static/media/legf_1.403b6680.gif"
+  },
+  {
+    "revision": "c3e1faacf307083eefc4918a5772a754",
+    "url": "/bausoft_website/static/media/legforgalom.c3e1faac.gif"
+  },
+  {
+    "revision": "3d6c1145fa4fa4376255912676a490af",
+    "url": "/bausoft_website/static/media/optimumegg.3d6c1145.png"
+  },
+  {
+    "revision": "d71ec1da9fe643378d976269f0d59497",
+    "url": "/bausoft_website/static/media/password.d71ec1da.gif"
+  },
+  {
+    "revision": "92248906b18af198303bedaa07dfd885",
+    "url": "/bausoft_website/static/media/password3.92248906.gif"
+  },
+  {
+    "revision": "f0bb06573833e1c516dcb248890b6415",
+    "url": "/bausoft_website/static/media/pinty.f0bb0657.gif"
+  },
+  {
+    "revision": "7a0f26afa961a045a741773615ebcbf2",
+    "url": "/bausoft_website/static/media/rigo.7a0f26af.gif"
+  },
+  {
+    "revision": "d10d3de732da359f73a648a2281c8cf0",
+    "url": "/bausoft_website/static/media/rozmaring_logo24.d10d3de7.png"
+  },
+  {
+    "revision": "fefedf878c73b184f49725c18565a86c",
+    "url": "/bausoft_website/static/media/siraly.fefedf87.gif"
+  },
+  {
+    "revision": "26bb3eea4d88f61c1f89a45b31afc94d",
+    "url": "/bausoft_website/static/media/sunarch.26bb3eea.jpg"
+  },
+  {
+    "revision": "c9adfab1c7868be0830a1aca959408bf",
+    "url": "/bausoft_website/static/media/sunarch_1.c9adfab1.gif"
+  },
+  {
+    "revision": "81933e254081565c5d7040a4f96e0cc3",
+    "url": "/bausoft_website/static/media/sunarch_11.81933e25.gif"
+  },
+  {
+    "revision": "c6f14ec385c87e891fc45db467bf439a",
+    "url": "/bausoft_website/static/media/sunarch_12.c6f14ec3.gif"
+  },
+  {
+    "revision": "166c12f612cb768fe52ee4c3413dfc6e",
+    "url": "/bausoft_website/static/media/sunarch_2.166c12f6.gif"
+  },
+  {
+    "revision": "e03acd2f41d46239b6b0efdb211f2cd9",
+    "url": "/bausoft_website/static/media/sunarch_3.e03acd2f.gif"
+  },
+  {
+    "revision": "bebec64997fbccbe197189f1c3883b12",
+    "url": "/bausoft_website/static/media/sunarch_31.bebec649.gif"
+  },
+  {
+    "revision": "4b31e51f3a1da98a0958034dcdfef552",
+    "url": "/bausoft_website/static/media/sunarch_32.4b31e51f.gif"
+  },
+  {
+    "revision": "454e995392cc1f67fb074b9c20ff1392",
+    "url": "/bausoft_website/static/media/sunarch_4.454e9953.gif"
+  },
+  {
+    "revision": "ca7fe3e8ac55249f3ff2e7294741f556",
+    "url": "/bausoft_website/static/media/sunarch_5.ca7fe3e8.gif"
+  },
+  {
+    "revision": "0beb06937c78bb546e8527c0f583df29",
+    "url": "/bausoft_website/static/media/sunarch_6.0beb0693.png"
+  },
+  {
+    "revision": "becb43fba8589fac8dad600eea96381c",
+    "url": "/bausoft_website/static/media/sunarch_7.becb43fb.gif"
+  },
+  {
+    "revision": "c1570464dd2ed7fb5845498db49d5e8b",
+    "url": "/bausoft_website/static/media/watern2.c1570464.gif"
+  },
+  {
+    "revision": "38fe297cae12f108f93f0625020fb5c1",
+    "url": "/bausoft_website/static/media/watern32_1.38fe297c.png"
+  },
+  {
+    "revision": "4f0372a6f4c5beef18eddf9a5b277c70",
+    "url": "/bausoft_website/static/media/watern32_2.4f0372a6.png"
+  },
+  {
+    "revision": "ff5949a9e5193e04e442149ab4ea641c",
+    "url": "/bausoft_website/static/media/watern32_3.ff5949a9.png"
+  },
+  {
+    "revision": "8af4fa15122a59aaa77f80764a4a78de",
+    "url": "/bausoft_website/static/media/watern32_4.8af4fa15.png"
+  },
+  {
+    "revision": "667cf61fe5d3167947f81ac9e40b950c",
+    "url": "/bausoft_website/static/media/ww_ecs1.667cf61f.gif"
+  },
+  {
+    "revision": "fab9c4a0dbda895ad07244c999c81327",
+    "url": "/bausoft_website/static/media/ww_ecs2.fab9c4a0.gif"
+  },
+  {
+    "revision": "c0a2d153b70a1b674f829b6eb8f7e7c8",
+    "url": "/bausoft_website/static/media/ww_epbd1.c0a2d153.gif"
+  },
+  {
+    "revision": "60fd1c5802d6ace13c254f64a8ec6b8a",
+    "url": "/bausoft_website/static/media/ww_epbd2.60fd1c58.gif"
+  },
+  {
+    "revision": "2597faf3f4901aca0ff7e1a35d05927d",
+    "url": "/bausoft_website/static/media/ww_epbd3.2597faf3.gif"
+  },
+  {
+    "revision": "d91d0fa9b11a8b6b47b207ebe96273be",
+    "url": "/bausoft_website/static/media/ww_epbd4.d91d0fa9.gif"
+  },
+  {
+    "revision": "037b3b39457eecd295592e309edcb897",
+    "url": "/bausoft_website/static/media/ww_epf1.037b3b39.gif"
+  },
+  {
+    "revision": "0399efaa24fd0f20286717b61787f8d1",
+    "url": "/bausoft_website/static/media/ww_epf2.0399efaa.gif"
+  },
+  {
+    "revision": "d66d6372543d3cafd235b0f39503fe9e",
+    "url": "/bausoft_website/static/media/ww_hidr1.d66d6372.gif"
+  },
+  {
+    "revision": "ac21781989fc84f69c16b9d7256f6306",
+    "url": "/bausoft_website/static/media/ww_hidr2.ac217819.gif"
+  },
+  {
+    "revision": "e8254c9dac5d3cfdea1dcc224271566d",
+    "url": "/bausoft_website/static/media/ww_hidroplan.e8254c9d.png"
+  },
+  {
+    "revision": "e3f2512ec135cab3fc643d4f0925da1d",
+    "url": "/bausoft_website/static/media/ww_hocs1.e3f2512e.gif"
+  },
+  {
+    "revision": "a814461798d6add6ef8eec57f99f947a",
+    "url": "/bausoft_website/static/media/ww_nyar1.a8144617.gif"
+  },
+  {
+    "revision": "19df0f52ade3b093424ba8add9b18c64",
+    "url": "/bausoft_website/static/media/ww_nyar2.19df0f52.gif"
+  },
+  {
+    "revision": "a74aa3ebc3159b352051c65d01fd7b8e",
+    "url": "/bausoft_website/static/media/ww_optimum1.a74aa3eb.gif"
+  },
+  {
+    "revision": "e53afe4d888f783fc84b3e01033d1ea5",
+    "url": "/bausoft_website/static/media/ww_optimum2.e53afe4d.gif"
+  },
+  {
+    "revision": "db53b162716363b408ee1ae237df24da",
+    "url": "/bausoft_website/static/media/ww_optimum3.db53b162.gif"
+  },
+  {
+    "revision": "4eb7f80c35b16c8e75f517f61fb78672",
+    "url": "/bausoft_website/static/media/ww_optimum4.4eb7f80c.gif"
+  },
+  {
+    "revision": "15e0514092c5ec5c08375f1f632b9a33",
+    "url": "/bausoft_website/static/media/ww_pad1.15e05140.gif"
+  },
+  {
+    "revision": "8407d6f2a18366052a6097686f8953de",
+    "url": "/bausoft_website/static/media/ww_pad2.8407d6f2.gif"
+  },
+  {
+    "revision": "1a8609ceae60cfb57be4ebd9df986581",
+    "url": "/bausoft_website/static/media/ww_rad1.1a8609ce.gif"
+  },
+  {
+    "revision": "1b01434b70de0c6fdbc011bcd28b0938",
+    "url": "/bausoft_website/static/media/ww_rad2.1b01434b.gif"
+  },
+  {
+    "revision": "b10439becc310af8952c5a037118a885",
+    "url": "/bausoft_website/static/media/ww_sun1.b10439be.png"
+  },
+  {
+    "revision": "a5360320d83f53a15a6869e2dcd6a4f5",
+    "url": "/bausoft_website/static/media/ww_sun2.a5360320.png"
+  },
+  {
+    "revision": "be8db7b65a301e4d4133b1c4259aa36d",
+    "url": "/bausoft_website/static/media/ww_teli1.be8db7b6.gif"
+  },
+  {
+    "revision": "e69f60046548e684fed0e48a93dc8d35",
+    "url": "/bausoft_website/static/media/ww_teli2.e69f6004.gif"
+  },
+  {
+    "revision": "3c58a4208bc1c5983bd2204960b91ccf",
+    "url": "/bausoft_website/static/media/youtube.3c58a420.jpg"
+  }
+]);
